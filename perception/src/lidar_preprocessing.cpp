8ffe977d8f871dbf83e59fab3f9f147b89d94318
David Chung <76574620+chungdavid@users.noreply.github.com>
2024-03-09 19:11:57 -0500

    update lidar preprocessing pipeline to crop ROI

--- perception/src/lidar_preprocessing.cpp
+++ perception/src/lidar_preprocessing.cpp
@@ -8,6 +8,7 @@
 #include <pcl_conversions/pcl_conversions.h>
 
 #include <pcl/filters/voxel_grid.h>
+#include <pcl/filters/passthrough.h>
 
 
 class LidarPreprocessor {
@@ -15,19 +16,22 @@ private:
     ros::NodeHandle nh_;
     ros::Subscriber input_sub_;
     ros::Publisher output_pub_;
+    ros::Publisher output_pub_cropped_;
     laser_geometry::LaserProjection projector_;
-    std::string lidar_frame_; //we have to specify this at the beginnign (during preprocessing) because the topic we read the raw hokuyo scan from does not have a frame id
+    std::string lidar_frame_; //we have to specify this at the beginning (during preprocessing) because the topic we read the raw hokuyo scan from does not have a frame id
 
 public:
     LidarPreprocessor() {
         input_sub_ = nh_.subscribe("/scan", 1000, &LidarPreprocessor::lidarPreprocessingPipeline, this);
         output_pub_ = nh_.advertise<pcl::PCLPointCloud2>("/lidar/hokuyo/pointcloud2_preprocessed", 1);
+        output_pub_cropped_ = nh_.advertise<pcl::PCLPointCloud2>("/lidar/hokuyo/pointcloud2_preprocessed_cropped", 1);
         lidar_frame_ = "hokuyo";
     }
 
     void lidarPreprocessingPipeline(const sensor_msgs::LaserScan::ConstPtr& msg)
     {
         //convert LaserScan to PointCloud2
+        //outliers are already removed in this step
         sensor_msgs::PointCloud2 cloud;
         projector_.projectLaser(*msg, cloud);
         cloud.header.frame_id = lidar_frame_;
@@ -38,19 +42,37 @@ public:
         pcl::PCLPointCloud2* pcl_cloud = new pcl::PCLPointCloud2; 
         pcl_conversions::toPCL(cloud, *pcl_cloud);
         pcl::PCLPointCloud2ConstPtr pcl_cloud_ptr(pcl_cloud);
-        // ROS_INFO("Num points starting: %d",pcl_cloud->width * pcl_cloud->width);
+        // ROS_INFO("Num points starting: %d",pcl_cloud->width * pcl_cloud->height);
 
-        //voxel grid filter
+        //voxel grid filter for downsampling
         pcl::PCLPointCloud2::Ptr pcl_cloud_filtered_ptr(new pcl::PCLPointCloud2);
         pcl::VoxelGrid<pcl::PCLPointCloud2> sor;
         sor.setInputCloud (pcl_cloud_ptr);
-        sor.setLeafSize (0.01, 0.01, 0.01);
+        sor.setLeafSize (0.02, 0.02, 0.02); //2 cm
         sor.filter (*pcl_cloud_filtered_ptr);
-        // ROS_INFO("Num points after voxel filter: %d", cloud_filtered.width * cloud_filtered.width);
-
-        //pass through filter
+        // ROS_INFO("Num points after voxel filter: %d", cloud_filtered.width * cloud_filtered.height);
         
+        //crop the lidar data to a specific region
+        pcl::PCLPointCloud2::Ptr pcl_cloud_cropped_ptr(new pcl::PCLPointCloud2);
+        pcl::PassThrough<pcl::PCLPointCloud2> pass;
+        //filter along y
+        pass.setInputCloud(pcl_cloud_filtered_ptr); 
+        pass.setFilterFieldName ("y");
+        pass.setFilterLimits (-1.5, 1.5);
+        pass.filter (*pcl_cloud_cropped_ptr);
+        //filter along x
+        pass.setInputCloud(pcl_cloud_cropped_ptr); 
+        pass.setFilterFieldName ("x");
+        pass.setFilterLimits (-1, 5);
+        pass.filter (*pcl_cloud_cropped_ptr);
+        
+        //bilateral or gaussian filter?
+
+        //publish the preprocessed LiDAR data
         output_pub_.publish(*pcl_cloud_filtered_ptr);
+        //publish the preprocessed and cropped lidar data
+        output_pub_cropped_.publish(*pcl_cloud_cropped_ptr);
+        
     }
 };
 
